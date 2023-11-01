e3cad5a218bc8ddbdd238b03bbb97cd9fc1e5235
MichalStanaszek <michal.stanaszek@gmail.com>
2023-11-01 23:48:02 +0100

    Create LoginPage.jsx

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,18 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci
+        repellendus laborum possimus, quos ratione dolor soluta ut porro,
+        consequuntur dolores. Impedit iure deserunt eos libero obcaecati
+        inventore aperiam ea! Corporis, laboriosam officiis. Vero ex nihil quae
+        aperiam sunt consequatur, sed voluptatem dolores expedita blanditiis
+        autem quam voluptas tenetur, cumque suscipit error, quis quisquam harum
+        similique. Et ab atque cumque.
+      </p>
+    </div>
+  );
+};
+
+export default LoginPage;
