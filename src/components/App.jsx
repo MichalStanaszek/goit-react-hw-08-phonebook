5e4c4135a3c77c562115d0d31bc3c230f7fe3c0e
MichalStanaszek <michal.stanaszek@gmail.com>
2023-11-01 23:48:38 +0100

    Clear App.js for refactoring
    start adding routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,12 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts } from 'redux/operations';
-import { selectContacts, selectError } from 'redux/selectors';
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import { ContactList, Filter, Form, Error } from '../components/index';
-import { StyledWrapper, StyledH1, StyledP } from './App.styled';
+const LoginPage = lazy(() => import('../pages/LoginPage'));
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const error = useSelector(selectError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
   return (
-    <>
-      {error ? (
-        <Error />
-      ) : (
-        <StyledWrapper>
-          <StyledH1>Phonebook</StyledH1>
-          <StyledP>New contact</StyledP>
-          <Form />
-          <h2 style={{ textAlign: 'center' }}>Contacts: {contacts.length}</h2>
-          <Filter />
-          <ContactList />
-        </StyledWrapper>
-      )}
-    </>
+    <Routes>
+      <Route index element={<LoginPage />} />
+    </Routes>
   );
 };
