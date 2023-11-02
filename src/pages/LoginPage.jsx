6078cba62acbd219636c77d6d94bec0416477b3c
MichalStanaszek <michal.stanaszek@gmail.com>
2023-11-03 00:06:10 +0100

    Create LoginForm.jsx (component)
    - create login component
    - use LoginForm in LoginPage
    - create styles for LoginPage
    - download LoginPage backgroung picture

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,16 +1,13 @@
+import { LoginForm } from 'components/LoginForm/LoginForm';
+import { Helmet } from 'react-helmet';
+
 const LoginPage = () => {
   return (
     <div>
-      <h1>Login Page</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci
-        repellendus laborum possimus, quos ratione dolor soluta ut porro,
-        consequuntur dolores. Impedit iure deserunt eos libero obcaecati
-        inventore aperiam ea! Corporis, laboriosam officiis. Vero ex nihil quae
-        aperiam sunt consequatur, sed voluptatem dolores expedita blanditiis
-        autem quam voluptas tenetur, cumque suscipit error, quis quisquam harum
-        similique. Et ab atque cumque.
-      </p>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <LoginForm />
     </div>
   );
 };
