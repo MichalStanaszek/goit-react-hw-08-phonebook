ffab95067004cb09509b0aa95e5b71e2a7916762
MichalStanaszek <michal.stanaszek@gmail.com>
2023-11-02 20:26:54 +0100

    Add basic HomePage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 const LoginPage = lazy(() => import('../pages/LoginPage'));
+const HomePage = lazy(() => import('../pages/HomePage'));
 
 export const App = () => {
   return (
     <Routes>
-      <Route index element={<LoginPage />} />
+      <Route index element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Welcome in your Phonebook app! Place where you can keep and manage all
+        your contacts.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
