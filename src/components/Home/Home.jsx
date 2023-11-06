11138ed6381ff30cbe9d14c3f9809891c8651c37
MichalStanaszek <michal.stanaszek@gmail.com>
2023-11-06 21:44:49 +0100

    edit Home component
    move nav section to the SharedLayout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,13 @@
 // import { Link } from 'react-router-dom';
-import { Header, Nav, Link, H1, Img } from './Home.styled';
+import { H1, Img, StyledDiv } from './Home.styled';
 
 import home from './home.jpg';
 
 export const Home = () => {
   return (
-    <>
-      <Header>
-        <Nav>
-          <div>
-            <Link to="/">Home</Link>
-          </div>
-          <div>
-            <Link to="/login">Login</Link>
-          </div>
-        </Nav>
-      </Header>
-      <div>
-        <H1>Welcome in PhoneBook app! Your new place to manage contacts.</H1>
-        <Img src={home} alt="home" />
-      </div>
-    </>
+    <StyledDiv>
+      <H1>Welcome in PhoneBook app! Your new place to manage contacts.</H1>
+      <Img src={home} alt="home" />
+    </StyledDiv>
   );
 };
--- src/components/Home/Home.styled.js
+++ src/components/Home/Home.styled.js
@@ -1,33 +1,12 @@
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
 
-export const Nav = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 20px;
-`;
-
-export const Header = styled.header`
-  padding: 8px 0;
-  margin-bottom: 16px;
-  border-bottom: 1px solid black;
-`;
-
-export const Link = styled(NavLink)`
-  padding: 8px 16px;
-  border-radius: 4px;
-  text-decoration: none;
-  color: black;
-  font-weight: 500;
-
-  &.active {
-    color: white;
-    background-color: #694718;
-  }
+export const StyledDiv = styled.div`
+  background-color: #fff;
 `;
 
 export const H1 = styled.h1`
   text-align: center;
+  padding: 10px 0;
 `;
 
 export const Img = styled.img`
