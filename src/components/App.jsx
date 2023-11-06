3cb722d2bc8cf0a606fd0e7913fc5bb92e8c13fc
MichalStanaszek <michal.stanaszek@gmail.com>
2023-11-06 21:45:57 +0100

    edit LoginForm and LoginPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from './index';
 
-const LoginPage = lazy(() => import('../pages/LoginPage'));
 const HomePage = lazy(() => import('../pages/HomePage'));
+const LoginPage = lazy(() => import('../pages/LoginPage'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
