c8bbaee6aa2365708d21a056275a128368e4d1af
MichalStanaszek <michal.stanaszek@gmail.com>
2023-11-08 21:33:12 +0100

    Create authSlice.js and connect with store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
+import { authReducer } from './auth/authSlice';
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
+    auth: authReducer,
   },
 });
