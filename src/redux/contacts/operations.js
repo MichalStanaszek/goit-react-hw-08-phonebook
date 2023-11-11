f7918734569fd1e8e00713f1be2c678aa8487e99
MichalStanaszek <michal.stanaszek@gmail.com>
2023-11-11 19:37:15 +0100

    remove old axios.defaults.baseURL
    old API: mockapi

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://653047a56c756603295e803b.mockapi.io';
-
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
