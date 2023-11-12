09819a75489763bdd62b5977da7e463a3d775685
MichalStanaszek <122054995+MichalStanaszek@users.noreply.github.com>
2023-11-12 11:22:46 +0000

    Deploying to gh-pages from @ MichalStanaszek/goit-react-hw-08-phonebook@f7a6d95136c15e8ca60b47e7d980a18b6c3e88b8 🚀

---
+++ static/js/983.abcd2042.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,r){r.r(t);var n=r(1660),c=r(4270),o=r(3329);t.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)(c.q,{children:(0,o.jsx)("title",{children:"Phonebook - Home"})}),(0,o.jsx)(n.SK,{})]})}}}]);
+//# sourceMappingURL=983.abcd2042.chunk.js.map
